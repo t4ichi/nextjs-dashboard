382a8ff119add050a8870f184099471a6fee07fa
t4ichi <taichinoheya020316@icloud.com>
2024-09-01 02:38:42 +0900

    add chapter12

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,11 +4,22 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
+  
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+  const allPages = generatePagination(currentPage, totalPages);
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const createPageURL = (page: number | string) => {
+    const search = new URLSearchParams(searchParams);
+    search.set('page', String(page));
+    return `${pathname}?${search.toString()}`;
+  };
+
 
   return (
     <>
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+export const experimental_ppr = true;
+const nextConfig = {
+  experimental: {
+    ppr: 'incremental',
+  },
+};
 
 export default nextConfig;
